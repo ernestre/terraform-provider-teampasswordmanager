b8d9fa858054d275379d06b7643957f5ddfec3e6
Ernest Rekel <ernest.rekel@gmail.com>
2022-06-13 21:56:50 +0300

    Fix docs generate and add more examples

---
+++ examples/resources/teampasswordmanager_password/resource.tf
@@ -0,0 +1,20 @@
+resource "teampasswordmanager_project" "new" {
+  name  = "wordpress"
+  notes = "wordpress secrets"
+  tags = [
+    "e-shop",
+    "wp",
+  ]
+}
+
+resource "teampasswordmanager_password" "database" {
+  name       = "database"
+  password   = "feechu0W"
+  project_id = teampasswordmanager_project.new.id
+}
+
+resource "teampasswordmanager_password" "sendgrid" {
+  name       = "sendgrid"
+  password   = "az4Oowis"
+  project_id = teampasswordmanager_project.new.id
+}
