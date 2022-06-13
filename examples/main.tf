6e27db95971f888349d64bc0fcf6e5887a2bcc9e
Ernest Rekel <ernest.rekel@gmail.com>
2022-06-13 21:14:43 +0300

    Fix examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     teampasswordmanager = {
-      version = "0.2.0"
-      source  = "hashicorp.com/edu/teampasswordmanager"
+      source  = "ernestre/teampasswordmanager"
+      version = "0.1.2"
     }
   }
 }
